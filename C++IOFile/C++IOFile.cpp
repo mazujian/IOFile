a94a7a6104ece95e2a45f5066b7e4aee429698be
hello <1477962984@qq.com>
2020-04-26 20:34:52 +0800

    1

--- C++IOFile/C++IOFile.cpp
+++ C++IOFile/C++IOFile.cpp
@@ -3,9 +3,135 @@
 
 #include <iostream>
 
+#define MAX 1024
+typedef int(*Fun)(char* s,FILE *file);
+
+int chooseAction(Fun fun, char* name)
+{
+	FILE *file = fopen("D:\\MyProject\\c++\\FILE\\bookname.txt", "a+");
+	fun(name,file);
+	return 0;
+}
+
+int addFile(char *pName, FILE *file)
+{
+	char bookname[100] = {0};
+	char autor[100] = { 0 };
+	char press[100] = { 0 };
+	//char string[MAX] = " ";
+	
+	if (file == NULL)
+	{
+		printf("读取文件失败");
+		return -1;
+	}
+	printf("请输入要添加的书名：");
+	scanf("%s", bookname);
+	printf("请输入要添加的书的作者：");
+	scanf("%s", autor);
+	printf("请输入要添加的出版社：");
+	scanf("%s", press);
+	//strcat_s(string, sizeof(string), bookname);
+	//strcat_s(string, sizeof(string), autor);
+	//strcat_s(string, sizeof(string), press);
+	fprintf(file, "%-20s%-20s%-20s\n", bookname, autor, press);
+	//fputs(string, file);
+	printf("添加完完成\n");
+	/*
+	while (fgets(string, MAX, file) != NULL)
+	{
+
+	}
+	*/
+	fclose(file);
+	return 0;
+}
+
+int upDateFile(char *pName, FILE *file)
+{
+
+	return 0;
+}
+
+int selectFile(char *pName, FILE *file)
+{
+	char userStr[MAX] = { 0 }; 
+	char sysStr[MAX] = { 0 };
+	char sysString[MAX] = { 0 };
+	printf("请输入要查找的书名或者作者\n");
+	scanf("%s", userStr);
+	while (fgets(sysStr,MAX,file) != NULL)
+	{
+		int i = 0;
+		int j = 0;
+		while (i < strlen(sysStr))
+		{
+			if (sysStr[i] == '\t'||sysStr[i] == '\n'||sysStr[i] == ' ') //因为记事本使用了空格（' '）与水平制表符（'\t'）,所以都要判断
+			{
+				if (strcmp(userStr, sysString) == 0)
+				{
+					printf("找到了\n");
+					printf("%s \n", sysStr);
+					return 0;
+				}
+				memset(sysString, 0, sizeof(sysString));
+				j = 0;
+			}
+			else
+			{
+				sysString[j++] = sysStr[i];
+			}
+			i++;
+		}
+	}
+	printf("没有找到\n");
+	return 0;
+}
+int deleteFile(char *pName, FILE *file)
+{
+	return 0;
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+#if 1
+	int offOn = 1;
+	int* pAction;
+	int(*arrFun[4])(char *s, FILE *file) = { selectFile,addFile,upDateFile,deleteFile };
+	printf("0.查询图书：\n");
+	printf("1.增加图书：\n");
+	printf("2.修改图书：\n");
+	printf("3.删除图书：\n");
+	//printf("5.返回上一层：");
+	printf("6.退出：\n");
+	
+	while (offOn != 6)
+	{
+
+		printf("请输入操作类型的序列号：");
+		//这里如果输入了遗传字符串scanf不会给offon赋值，也不会刷新缓存区，导致死循环，加个getchar刷新缓存区
+		int a = scanf("%d", &offOn);
+		if (offOn < 6 && offOn >= 0 )
+		{
+			//printf("操作文件");
+			char name[] = " ";
+			chooseAction(arrFun[offOn],name);
+		}
+		else
+		{
+			//fflush(stdin); c11之后失效
+			getchar();
+			printf("无效输入，请从新输入 \n");
+		}
+	}
+#endif
+#if 0
+	char ch;
+	while ((ch = getchar()) != '#')
+	{
+		putchar(ch);
+	}
+#endif
+	return 0;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
